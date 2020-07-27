8b0a283e1604c2536cd6863e2cc803b512a39278
danielnwaroh <daniel.nwaroh@ucalgary.ca>
2020-07-27 17:51:13 -0600

    stuff

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -15,11 +15,6 @@ import {
 const classes = (theme) => ({
   root: {
     position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    maxHeight: 200,
-    height: "100%",
-    justifyContent: "space-between",
   },
   bottom: {
     color: theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
@@ -49,7 +44,7 @@ class MoviePage extends React.Component {
     console.log(window.location.href);
     let currUrl = window.location.href;
     let paramQ = currUrl.split("search?")[1];
-    console.log(paramQ);
+    // console.log(paramQ);
     fetch("https://imdb8.p.rapidapi.com/title/find?q=" + paramQ, {
       method: "GET",
       headers: {
@@ -59,9 +54,9 @@ class MoviePage extends React.Component {
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         let movies = response.results;
-        console.log(movies);
+        // console.log(movies);
         let tempFiltered = movies.filter(
           (movie) => movie.titleType === "movie"
         );
@@ -95,51 +90,59 @@ class MoviePage extends React.Component {
               />
             </div>
           ) : (
-            // <img
-            //   src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-            //   alt="new"
-            // />
-            //   {listOfMovies.map((course, index) => (
-            //         <div>hi</div>
-            //     ))}
             <Grid item xs={12}>
               <Grid container justify="center" spacing={2}>
-                {listOfMovies.map(({ title, id }) => (
-                  <Grid item={true} xs={12} sm={4} key={id}>
-                    <Card className={classes.root}>
-                      <CardActionArea>
-                        <CardMedia
-                          className={classes.media}
-                          component="img"
-                          src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-                          title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {title}
-                          </Typography>
-                          <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                          >
-                            Lizards are a widespread group of squamate reptiles,
-                            with over 6,000 species, ranging across all
-                            continents except Antarctica
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          Share
-                        </Button>
-                        <Button size="small" color="primary">
-                          Learn More
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
+                {listOfMovies.map(
+                  ({ title, id, image, runningTimeInMinutes, principals }) => (
+                    <Grid item={true} xs={12} sm={4} key={id}>
+                      <Card className={classes.root}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.media}
+                            style={{ height: "525px" }}
+                            component="img"
+                            src={image.url}
+                            title="Contemplative Reptile"
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="h2"
+                            >
+                              {title}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              Starring: {principals[0].name} as{" "}
+                              {principals[0].characters.join("/")} and{" "}
+                              {principals[1].name} as{" "}
+                              {principals[1].characters.join("/")}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              Runtime: {runningTimeInMinutes} minutes
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                          <Button size="small" color="primary">
+                            Learn More
+                          </Button>
+                          <Button size="small" color="primary">
+                            Share
+                          </Button>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  )
+                )}
               </Grid>
             </Grid>
           )}
